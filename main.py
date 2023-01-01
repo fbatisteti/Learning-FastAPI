cbbc557d3645da9f6195d648a3e689902d78dade
Fabio Batisteti <fbatisteti@hotmail.com>
2023-01-01 19:06:41 -0300

    Quickfix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import engine, Base, get_db
+from database import engine, Base
 from models import carro, cliente, locacao
 from routes import carro, cliente, locacao
 
